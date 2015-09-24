1e3d17fce06de03ac1b9b2da53bb5e19122878b7
totalwar235 <rbnorstrom@pluto.dsu.edu>
2015-09-24 17:16:24 -0500

    primality done

--- src/algor/assignment/two/AlgorAssignmentTwo.java
+++ src/algor/assignment/two/AlgorAssignmentTwo.java
@@ -5,7 +5,7 @@
  */
 package algor.assignment.two;
 
-import static javafx.beans.binding.Bindings.convert;
+import java.util.Random;
 
 /**
  *
@@ -17,11 +17,31 @@ public class AlgorAssignmentTwo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Primality k = new Primality(15485863);
-        long temp = k.User_Alg_Check();
+        boolean primality = true;
         
-        System.out.println("time: "+String.valueOf(temp) + " :: Prime: " + String.valueOf(k.prime));
+        for (int i = 100; i > 0; i--) {
+
+            Random rand = new Random();
+
+            int randomNum = rand.nextInt();
+
+            Primality k = new Primality(randomNum);
+            long temp = k.User_Alg_Check();
+
+            System.out.println(String.valueOf(i)+": " +String.valueOf(randomNum));
+            System.out.println("HomeBrew ~ time: " + String.valueOf(temp) + " :: Prime: " + String.valueOf(k.hbprime));
+            
+            temp = k.isPrime2();
+            
+            System.out.println("Documented ~ time: " + String.valueOf(temp) + " :: Prime: " + String.valueOf(k.prime) +"\n");
+            
+            if(k.prime != k.hbprime){
+               System.out.println("\n\n\n There is an incorrect Result! \n\n\n"); 
+               primality = false;
+            }
+            
+            
+        }
     }
-    
+
 }
--- src/algor/assignment/two/Primality.java
+++ src/algor/assignment/two/Primality.java
@@ -12,32 +12,80 @@ package algor.assignment.two;
 public class Primality {
 
     int User;
+    boolean hbprime;
     boolean prime;
 
     public Primality(int x) {
         User = x;
     }
 
+    /*
+     * Home Brewed Solution 
+     */
     public Long User_Alg_Check() {
         long startTime = System.currentTimeMillis();
 
         int temp;
-        for (int x = 2; x < User; x++) {
+        if ((User % 2) == 0) {
+            hbprime = false;
+
+            long duration = 0;
+            return duration;
+        }
+        for (int x = 3; x < User; x += 2) {
             temp = User % x;
             if (temp == 0) {
-                prime = false;
+                hbprime = false;
 
                 long endTime = System.currentTimeMillis();
 
-                long duration = (endTime - startTime);  
+                long duration = (endTime - startTime);
                 return duration;
             }
         }
 
         long endTime = System.currentTimeMillis();
 
-        long duration = (endTime - startTime); 
+        long duration = (endTime - startTime);
+
+        hbprime = true;
+
+        return duration;
+    }
+
+    /*
+    *Recorded Primality test from the internet
+    */
+    public Long isPrime2() {
+
+        long startTime = System.currentTimeMillis();
         
+        
+        if (User == 2 || User == 1 || User == 0) {
+            prime = true;
+            long endTime = System.currentTimeMillis();
+            long duration = (endTime - startTime);
+            return duration;
+        }
+        if (User % 2 == 0) {
+            prime = false;
+            long endTime = System.currentTimeMillis();
+            long duration = (endTime - startTime);
+            return duration;
+        }
+        for (int i = 3; i <= Math.sqrt(User) + 1; i = i + 2) {
+            if (User % i == 0) {
+                prime = false;
+                long endTime = System.currentTimeMillis();
+                long duration = (endTime - startTime);
+                return duration;
+            }
+        }
+
+        long endTime = System.currentTimeMillis();
+
+        long duration = (endTime - startTime);
+
         prime = true;
 
         return duration;

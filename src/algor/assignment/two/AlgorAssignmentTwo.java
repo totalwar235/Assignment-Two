98b90db70630fbc0fed6235749772b1f58787062
totalwar235 <rbnorstrom@pluto.dsu.edu>
2015-09-29 19:40:59 -0500

    finished primality testing

--- src/algor/assignment/two/AlgorAssignmentTwo.java
+++ src/algor/assignment/two/AlgorAssignmentTwo.java
@@ -17,8 +17,13 @@ public class AlgorAssignmentTwo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        primeTesting();
+    }
+
+    public static void primeTesting() {
         boolean primality = true;
-        
+        long avgHB = 0;
+        long avgDoc = 0;
         for (int i = 100; i > 0; i--) {
 
             Random rand = new Random();
@@ -27,21 +32,56 @@ public class AlgorAssignmentTwo {
 
             Primality k = new Primality(randomNum);
             long temp = k.User_Alg_Check();
+            avgHB += temp;
+            //System.out.println(String.valueOf(i)+": " +String.valueOf(randomNum));
+            //System.out.println("HomeBrew ~ time: " + String.valueOf(temp) + " :: Prime: " + String.valueOf(k.hbprime));
 
-            System.out.println(String.valueOf(i)+": " +String.valueOf(randomNum));
-            System.out.println("HomeBrew ~ time: " + String.valueOf(temp) + " :: Prime: " + String.valueOf(k.hbprime));
-            
             temp = k.isPrime2();
-            
-            System.out.println("Documented ~ time: " + String.valueOf(temp) + " :: Prime: " + String.valueOf(k.prime) +"\n");
-            
-            if(k.prime != k.hbprime){
-               System.out.println("\n\n\n There is an incorrect Result! \n\n\n"); 
-               primality = false;
+            avgDoc += temp;
+
+            //System.out.println("Documented ~ time: " + String.valueOf(temp) + " :: Prime: " + String.valueOf(k.prime) +"\n");
+            if (k.prime != k.hbprime) {
+                System.out.println("\n\n\n There is an incorrect Result! \n\n\n");
+                primality = false;
             }
-            
-            
+
         }
+
+        Primality k = new Primality(1);
+        System.out.println("Number: " + String.valueOf(k.User) + "\n\tHomeBrewed ~ time: " + String.valueOf(k.User_Alg_Check()) + " \n\tDocumented ~ time: " + String.valueOf(k.isPrime2()) + "\n");
+
+        k = new Primality(13);
+        System.out.println("Number: " + String.valueOf(k.User) + "\n\tHomeBrewed ~ time: " + String.valueOf(k.User_Alg_Check()) + " \n\tDocumented ~ time: " + String.valueOf(k.isPrime2()) + "\n");
+
+        k = new Primality(15485863);
+        System.out.println("Number: " + String.valueOf(k.User) + "\n\tHomeBrewed ~ time: " + String.valueOf(k.User_Alg_Check()) + " \n\tDocumented ~ time: " + String.valueOf(k.isPrime2()) + "\n");
+
+        k = new Primality(32452843);
+        System.out.println("Number: " + String.valueOf(k.User) + "\n\tHomeBrewed ~ time: " + String.valueOf(k.User_Alg_Check()) + " \n\tDocumented ~ time: " + String.valueOf(k.isPrime2()) + "\n");
+
+        k = new Primality(49979687);
+        System.out.println("Number: " + String.valueOf(k.User) + "\n\tHomeBrewed ~ time: " + String.valueOf(k.User_Alg_Check()) + " \n\tDocumented ~ time: " + String.valueOf(k.isPrime2()) + "\n");
+
+        k = new Primality(67867967);
+        System.out.println("Number: " + String.valueOf(k.User) + "\n\tHomeBrewed ~ time: " + String.valueOf(k.User_Alg_Check()) + " \n\tDocumented ~ time: " + String.valueOf(k.isPrime2()) + "\n");
+
+        k = new Primality(104395301);
+        System.out.println("Number: " + String.valueOf(k.User) + "\n\tHomeBrewed ~ time: " + String.valueOf(k.User_Alg_Check()) + " \n\tDocumented ~ time: " + String.valueOf(k.isPrime2()) + "\n");
+
+        k = new Primality(256203161);
+        System.out.println("Number: " + String.valueOf(k.User) + "\n\tHomeBrewed ~ time: " + String.valueOf(k.User_Alg_Check()) + " \n\tDocumented ~ time: " + String.valueOf(k.isPrime2()) + "\n");
+
+        k = new Primality(512927357);
+        System.out.println("Number: " + String.valueOf(k.User) + "\n\tHomeBrewed ~ time: " + String.valueOf(k.User_Alg_Check()) + " \n\tDocumented ~ time: " + String.valueOf(k.isPrime2()) + "\n");
+
+        k = new Primality(756065159);
+        System.out.println("Number: " + String.valueOf(k.User) + "\n\tHomeBrewed ~ time: " + String.valueOf(k.User_Alg_Check()) + " \n\tDocumented ~ time: " + String.valueOf(k.isPrime2()) + "\n");
+
+        k = new Primality(982451653);
+        System.out.println("Number: " + String.valueOf(k.User) + "\n\tHomeBrewed ~ time: " + String.valueOf(k.User_Alg_Check()) + " \n\tDocumented ~ time: " + String.valueOf(k.isPrime2()) + "\n");
+
+        System.out.println("100 random numbers\nHomebrewed avg time: " + String.valueOf(avgHB / 100) + "\nDocumented avg time: " + String.valueOf(avgDoc / 100) + "\n");
+
     }
 
 }
--- src/algor/assignment/two/Factoring.java
+++ src/algor/assignment/two/Factoring.java
@@ -5,13 +5,45 @@
  */
 package algor.assignment.two;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author totalwar
  */
 public class Factoring {
 
-    public Factoring() {
+    long base;
+    long timePrimeHB;
+
+    public Factoring(long b) {
+        base = b;
     }
-    
+
+    public List<Long> primeFactors() {
+        long startTime = System.currentTimeMillis();
+        List<Long> factors = new ArrayList<>();
+        long n;
+        n = this.base;
+        while (n % 2 == 0 && n > 0) {
+            factors.add(2L);
+            n /= 2;
+        }
+
+        for (long i = 3; i * i <= n; i += 2) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+        if (n > 1) {
+            factors.add(n);
+        }
+
+        long endTime = System.currentTimeMillis();
+
+        timePrimeHB = (endTime - startTime);
+        return factors;
+    }
 }
